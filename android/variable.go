0b500002d2d5f246d6151eed0cd310f3247f3d3a
Simon Shields <simon@lineageos.org>
2017-09-04 21:09:44 +1000

    soong: explicitly specify name of Cardinal variables struct
    Change-Id: Ic754b8a13658ddc40620f985581bd1754b780b15

--- android/variable.go
+++ android/variable.go
@@ -89,7 +89,7 @@ type variableProperties struct {
 		}
 
 		// include Cardinal variables
-		*android.Product_variables
+		Cardinal android.Product_variables
 	} `android:"arch_variant"`
 }
 
@@ -157,7 +157,7 @@ type productVariables struct {
 	Override_rs_driver *string `json:",omitempty"`
 
 	// include Cardinal variables
-	*android.ProductVariables
+	Cardinal android.ProductVariables
 }
 
 func boolPtr(v bool) *bool {
